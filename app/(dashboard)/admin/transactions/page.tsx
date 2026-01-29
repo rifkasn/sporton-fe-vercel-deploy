049a9e2247b59a707dd21e2f1b1bb8639401f4f8
rifkasn <rifkasn@gmail.com>
2026-01-29 21:44:48 +0700

    update: Integrasi Admin Panel & Backend

--- app/(dashboard)/admin/transactions/page.tsx
+++ app/(dashboard)/admin/transactions/page.tsx
@@ -4,18 +4,58 @@ import Button from "@/app/(landing)/components/ui/button";
 import { FiPlus } from "react-icons/fi";
 import TransactionTable from "../../components/transactions/transaction-table";
 import TransactionModal from "../../components/transactions/transaction-modal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Transaction } from "@/app/types";
+import { getAllTransactions, updateTransaction, } from "@/app/services/transaction.service";
+import { toast } from "react-toastify";
 
 const TransactionManagement = () => {
-    const [isOpen, setIsOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedTransaction, setSelectedTransaction] = useState<Transaction | null>(null);
+    const [transaction, setTransaction] = useState<Transaction[]>([]);
+
+    const fetchTransactions = async () => {
+        try {
+            const data = await getAllTransactions();
+            setTransaction(data);
+        } catch (error) {
+            console.error("Failed to fetch transactions", error);
+        }
+    };
 
     const handleCloseModal = () => {
-        setIsOpen(false);
+        setIsModalOpen(false);
+        setSelectedTransaction(null);
     };
 
-    const handleViewDetails = () => {
-        setIsOpen(true);
+    const handleViewDetails = (transaction: Transaction) => {
+        setIsModalOpen(true);
+        setSelectedTransaction(transaction);
     };
+
+    const handleStatusChange = async (
+        id: string,
+        status: "paid" | "rejected",
+    ) => {
+        try {
+            const formData = new FormData();
+            formData.append("status", status);
+            await updateTransaction(id, formData);
+
+            toast.success("Transaction status updated");
+
+            await fetchTransactions();
+        } catch (error) {
+            console.error("Failed to update transaction status", error);
+            toast.error("Failed to update transaction status");
+        } finally {
+            setIsModalOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchTransactions();
+    }, []);
 
     return (
         <div>
@@ -27,8 +67,14 @@ const TransactionManagement = () => {
                     </p>
                 </div>
             </div>
-            <TransactionTable onViewDetails={handleViewDetails} />
-            <TransactionModal isOpen={isOpen} onClose={handleCloseModal} />
+            <TransactionTable
+                transactions={transactions} 
+                onViewDetails={handleViewDetails} />
+            <TransactionModal 
+                transactions={transactions}
+                onStatusChange={handleStatusChange}
+                isOpen={isModalOpen} 
+                onClose={handleCloseModal} />
         </div>
     );
 };
