297cadf82ba36baef5cbde674b27d8b0e3c078c5
rifkasn <rifkasn@gmail.com>
2026-02-01 17:37:51 +0700

    update: Integrasi & typo bank

--- app/(dashboard)/admin/bank-info/page.tsx
+++ app/(dashboard)/admin/bank-info/page.tsx
@@ -5,27 +5,30 @@ import { FiPlus } from "react-icons/fi";
 import { useEffect, useState } from "react";
 import BankInfoList from "../../components/bank-info/bank-info-list";
 import BankInfoModal from "../../components/bank-info/bank-info-modal";
-import { getAllBanks } from "@/app/services/bank.service";
+import { Bank } from "@/app/types";
+import { deleteBank, getAllBanks } from "@/app/services/bank.service";
+import { toast } from "react-toastify";
+import DeleteModal from "../../components/ui/delete-modal";
 
 const BankInfoManagement = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [banks, setBanks] = useState<Bank[]>([]);
-  const [selectBank, setSelectBank] = useState<Bank | null> (null);
+  const [selectedBank, setSelectedBank] = useState<Bank | null>(null);
 
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const [bankToDeleteId, setBankToDeleteId] = useState("");
 
   const fetchBanks = async () => {
     try {
-        const data = await getAllBanks();
-        setBanks(data);
+      const data = await getAllBanks();
+      setBanks(data);
     } catch (error) {
-        console.error("Failed to fetch bank data", error);
+      console.error("Failed to fetch bank data", error);
     }
   };
 
   const handleCloseModal = () => {
-    setModalIsOpen(false);
+    setIsModalOpen(false);
     setSelectedBank(null);
   };
 
@@ -34,23 +37,23 @@ const BankInfoManagement = () => {
     setIsModalOpen(true);
   };
 
-  const handlDelete = (id: string) => {
-    setSelectedBank(id);
-    setIsModalOpen(true);
+  const handleDelete = (id: string) => {
+    setBankToDeleteId(id);
+    setIsDeleteModalOpen(true);
   };
 
-  const handlDeletConfirm = async () => {
+  const handlDeleteConfirm = async () => {
     if (!bankToDeleteId) return;
 
     try {
-        await deleteBank(bankToDeleteId);
-        toast.success("Bank info deleted succesfully");
-        setBankToDeleteId("");
-        setIsDeleteModalOpen(false);
-        fetchBanks();
+      await deleteBank(bankToDeleteId);
+      toast.success("Bank info deleted succesfully");
+      setBankToDeleteId("");
+      setIsDeleteModalOpen(false);
+      fetchBanks();
     } catch (error) {
-        console.error("Failed to delete bank info");
-        toast.error("Failed to delete bank info");
+      console.error("Failed to delete bank info");
+      toast.error("Failed to delete bank info ");
     }
   };
 
@@ -58,32 +61,34 @@ const BankInfoManagement = () => {
     fetchBanks();
   }, []);
 
-    return (
+  return (
+    <div>
+      <div className="flex justify-between items-center mb-10">
         <div>
-            <div className="flex justify-between items-center mb-10">
-            <div>
-                <h1 className="font-bold text-2xl">Bank Info Management</h1>
-                <p className="opacity-50">
-                    Manage destination accounts for customer transfers.
-                </p>
-            </div>
-            <Button className="rounded-lg" onClick={() =>setIsModalOpen(true)}>
-                <FiPlus size={24} />
-                Add Bank Account
-            </Button>
-            </div>
-            <BankInfoList banks={banks} onEdit={handlEdit} onDelete={handleDelete} />
-            <BankInfoModal 
-            isOpen={isModalOpen}
-            onSuccess={fetchBanks}
-            onClose={handleCloseModal}
-            bank={selectedBank} />
-            <DeleteModal
-            isOpen={isDeleteModalOpen}
-            onClose={() => setIsDeleteOpen(false)}
-            onConfirm={handleDeleteConfirm} />
-        </div> 
-    );
+          <h1 className="font-bold text-2xl">Bank Info Management</h1>
+          <p className="opacity-50">
+            Manage destination accounts for customer transfers.
+          </p>
+        </div>
+        <Button className="rounded-lg" onClick={() => setIsModalOpen(true)}>
+          <FiPlus size={24} />
+          Add Bank Account
+        </Button>
+      </div>
+      <BankInfoList banks={banks} onEdit={handlEdit} onDelete={handleDelete} />
+      <BankInfoModal
+        isOpen={isModalOpen}
+        onSuccess={fetchBanks}
+        onClose={handleCloseModal}
+        bank={selectedBank}
+      />
+      <DeleteModal
+        isOpen={isDeleteModalOpen}
+        onClose={() => setIsDeleteModalOpen(false)}
+        onConfirm={handlDeleteConfirm}
+      />
+    </div>
+  );
 };
 
 export default BankInfoManagement;
